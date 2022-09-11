13f697c343ae889b84285ea19e2a2235bb69c5ac
davealsina <davealsina@gmail.com>
2022-09-10 22:55:23 -0400

    huion driver for my digital table, and correction on install of spotify because of failure with packages keys

--- install_from_zero/debian_based/kali/install_all_apps.sh
+++ install_from_zero/debian_based/kali/install_all_apps.sh
@@ -93,9 +93,12 @@ rm $DOWNLOADS_FOLDER/discord.deb
 ##########################
 print_downloading_message  "spotify"
 
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install -y spotify-client
+#forgive me lord the apt keys were outdated and I wasn't able to find any solution yet
+sudo apt install snapd
+snap install spotify
+#curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+#echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+#sudo apt-get update && sudo apt-get install -y spotify-client
 
 
 #########################
