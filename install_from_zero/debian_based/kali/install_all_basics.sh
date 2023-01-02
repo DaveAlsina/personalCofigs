3186a1572b30128e49f8a3f675dcf506ed303ce5
DaveAlsina <davealsina@gmail.com>
2023-01-01 19:49:26 -0500

    addition, added dart instalation

--- install_from_zero/debian_based/kali/install_all_basics.sh
+++ install_from_zero/debian_based/kali/install_all_basics.sh
@@ -179,6 +179,18 @@ sudo cp ~/dotfiles/linodes /usr/local/bin
 sudo ln -s /usr/local/bin/linodes linodes
 
 
+############################
+# instala dart 
+############################
+cd 
+sudo apt-get install apt-transport-https
+wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/dart.gpg
+echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
+
+sudo apt-get update
+sudo apt-get install dart
+
+
 
 ###########################
 # instala latex completico
