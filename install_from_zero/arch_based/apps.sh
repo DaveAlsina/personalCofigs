e8893e3925dcbac7195158bbe1d3f8dc5a5ef3d5
DaveAlsina <davealsina@gmail.com>
2023-06-12 14:10:12 -0500

    update, added extra dependencies such as symbols and footprints for kicad

--- install_from_zero/arch_based/apps.sh
+++ install_from_zero/arch_based/apps.sh
@@ -28,6 +28,14 @@ paru -S --noconfirm zoom
 #-----------------------------
 paru -S --noconfirm  kicad thonny
 
+
+# Importing libraries and setting things up 
+sudo mkdir -p /usr/share/kicad/footprints/ && cd /usr/share/kicad/footprints/ && sudo wget https://gitlab.com/kicad/libraries/kicad-footprints/-/archive/master/kicad-footprints-master.zip && sudo unzip kicad-footprints-master.zip
+
+sudo mkdir -p /usr/share/kicad/symbols/ && cd /usr/share/kicad/symbols/ && sudo wget https://gitlab.com/kicad/libraries/kicad-symbols/-/archive/master/kicad-symbols-master.zip && sudo unzip kicad-symbols-master.zip
+
+sudo mkdir -p /usr/share/kicad/3dmodels/ && cd /usr/share/kicad/3dmodels/ && sudo wget https://gitlab.com/kicad/libraries/kicad-packages3D/-/archive/master/kicad-packages3D-master.zip && sudo unzip kicad-packages3D-master.zip
+
 #-----------------------------
 # Android Studio 
 #-----------------------------
